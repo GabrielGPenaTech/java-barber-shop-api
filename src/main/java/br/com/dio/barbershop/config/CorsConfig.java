730cdc5120cb5dddf6f66c3154830ef9d23f9c5f
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-10 07:37:37 -0300

    fix cors

--- src/main/java/br/com/dio/barbershop/config/CorsConfig.java
+++ src/main/java/br/com/dio/barbershop/config/CorsConfig.java
@@ -1,12 +1,12 @@
 package br.com.dio.barbershop.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Collections;
 
@@ -25,7 +25,7 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
-        bean.setFilter(new CorsFilter());
+        bean.setFilter(new CorsFilter(source));
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return  bean;
